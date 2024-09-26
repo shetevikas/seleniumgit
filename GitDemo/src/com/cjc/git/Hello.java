cd04fe832a1e198772b18b79fc5a15fea18be25f
shetevikas <shetevikas2006@gmail.com>
2024-09-26 09:44:12 +0530

    Update Hello.java

--- GitDemo/src/com/cjc/git/Hello.java
+++ GitDemo/src/com/cjc/git/Hello.java
@@ -2,6 +2,10 @@ package com.cjc.git;
 
 public class Hello 
 {
+  public void m1()
+  {
+    System.out.println("Its A m1 Method");	
+  }
 public static void main(String[] args) 
 {
 System.out.println("Welcome to Git");	
